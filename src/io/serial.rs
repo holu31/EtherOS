a2979dea9941511cf11984a6d9165d14e72793bc
pingvinya <pingvinya@192.168.0.64>
2023-12-18 19:14:06 +0300

    Уход от библиотеки uart_16550

--- src/io/serial.rs
+++ src/io/serial.rs
@@ -1,7 +1,7 @@
 use core::{fmt, fmt::Write};
 use lazy_static::lazy_static;
 use spin::Mutex;
-use uart_16550::SerialPort;
+use crate::io::ports::{inb, outb};
 
 pub static PORT_COM1: u16 = 0x3f8;
 pub static PORT_COM2: u16 = 0x2F8;
@@ -14,10 +14,67 @@ pub static PORT_COM8: u16 = 0x4E8;
 
 lazy_static! {
     pub static ref COM1: Mutex<SerialPort> = {
-        let mut serial_port = unsafe { SerialPort::new(PORT_COM1) };
-        serial_port.init();
+        let mut serial_port = SerialPort::new(PORT_COM1);
+        unsafe { serial_port.init(); }
         Mutex::new(serial_port)
     };
+}
+
+pub struct SerialPort {
+    port: u16
+}
+
+impl SerialPort {
+
+    pub fn new(port: u16) -> Self {
+        Self { port }
+    }
+
+    pub unsafe fn init(&mut self) -> bool {
+        outb(self.port + 1, 0x00);
+        outb(self.port + 3, 0x80);
+        outb(self.port + 0, 0x03);
+        outb(self.port + 1, 0x00);
+        outb(self.port + 3, 0x03);
+        outb(self.port + 2, 0xC7);
+        outb(self.port + 4, 0x0B);
+        outb(self.port + 4, 0x1E);
+        outb(self.port + 0, 0xAE);
+
+        if inb(self.port + 0) != 0xAE {
+            return false;
+        }
+
+        outb(self.port + 4, 0x0F);
+
+        true
+    }
+
+    unsafe fn is_transmit_empty(&mut self) -> u8 {
+        return inb(self.port + 5) & 0x20;
+    }
+
+    pub fn send_byte(&mut self, c: u8) {
+        unsafe { 
+            while self.is_transmit_empty() == 0 {}
+
+            outb(self.port, c)
+        }
+    }
+
+    pub fn send(&mut self, s: &str) {
+        for b in s.bytes() {
+            self.send_byte(b);
+        }
+    }
+}
+
+impl fmt::Write for SerialPort {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.send(s);
+
+        Ok(())
+    }
 }
 
 #[doc(hidden)]
@@ -42,9 +99,3 @@ macro_rules! serial_println {
     ($fmt:expr, $($arg:tt)*) => ($crate::serial_print!(
         concat!($fmt, "\n"), $($arg)*));
 }
-
-
-/*
-
-
-*/
